2d576e10139fa89fc967d901e0cd81fd7fdc0f18
leosarmento <leosarmento@ucl.br>
2023-02-09 16:33:32 -0300

    Day 3 of learning Vue, SPA this time

---
+++ src/components/NavbarLink.vue
@@ -0,0 +1,30 @@
+<template>
+    <a 
+        class="nav-link" 
+        :class="activeClasses" 
+        aria-current="page" 
+        :href="page.link.url"
+        :title="`This link goes to the ${page.link.text} page`"
+        @click.prevent="$bus.$emit('navbarLinkActived', index)"
+        >{{ page.link.text }}</a>
+</template>
+
+<script>
+export default {
+    props: ['page', 'index', 'isActive'],
+    computed: {
+        activeClasses() {
+            return {
+                active: this.isActive,
+                emphasize: this.isActive
+            }
+        }
+    }
+}
+</script>
+
+<style>
+.emphasize {
+    text-decoration: underline !important;
+}
+</style>
---
+++ src/main.js
@@ -0,0 +1,10 @@
+import { createApp, VueElement } from 'vue'
+import App from './App.vue'
+import '../node_modules/bootstrap/dist/css/bootstrap.css'
+import $bus from './utils/Events'
+
+const app = createApp(App)
+
+app.config.globalProperties.$bus = $bus;
+
+app.mount('#app')
