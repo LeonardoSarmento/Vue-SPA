f938ea90a369a4158268e8a0ca819ecb26c0f289
leosarmento <leosarmento@ucl.br>
2023-02-09 17:00:40 -0300

    Day 3 of learning Vue, SPA this time

--- src/components/PageViewer.vue
+++ src/components/PageViewer.vue
@@ -1,12 +1,14 @@
 <template>
     <div class="container">
-        <h1>{{ page.pageTitle }}</h1>
-        <p>{{ page.content }}</p>
+        <h1>Page Title</h1>
+        <p>{{$route.params.index}}</p>
     </div>
 </template>
 
 <script>
 export default {
-    props: ['page']
+    created(){
+
+    }
 }
 </script>
